2f0c6cb635f711ed60816adcb31a1d99cbb99281
Julioj1 <dbjulioj@gmail.com>
2025-10-03 10:50:59 -0400

    Fibonacci code

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 // Prints the first n Fibonacci numbers
 const printFibonacci = (n) => {
- 
+    let a = 0
+    let b = 1
+    for (let i = 0; i < n; i++) {
+        console.log(a);
+        let next = a + b; // add the current 2 number together.
+        a = b; // store the value of b into the variable a 
+        b = next; // store the value of next into the variable b
+    }
 }
 
 printFibonacci(5);  // should print 0, 1, 1, 2, 3
